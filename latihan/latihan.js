e48b87c77f3142429eb1a3737ede76c6896e69cf
MatiusStephen <matiusstephen813@gmail.com>
2023-11-07 09:55:39 +0700

    Menambah fitur timer pada latihan kana

--- latihan/latihan.js
+++ latihan/latihan.js
@@ -1,3 +1,47 @@
+let start = false
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let intervalId;
+
+function startTimer(){
+    intervalId = setInterval(function() {
+        // seconds++
+        // console.log(seconds)
+        updateTimer()
+    }, 1000);
+    start = true
+}
+
+function stopTimer(){
+    clearInterval(intervalId)
+    console.log(seconds)
+}
+
+const timerElement = document.body.querySelector('.timer')
+function updateTimer() {
+    // Increment the seconds
+    seconds++;
+
+    // Update minutes and reset seconds if necessary
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+
+    // Update hours and reset minutes if necessary
+    if (minutes === 60) {
+        minutes = 0;
+        hours++;
+    }
+
+    // Format hours, minutes, and seconds with leading zeros
+    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+    // Update the timer element
+    timerElement.textContent = formattedTime;
+}
+
 let choose;
 const container = document.body.querySelector('.container')
 const practiceBox = document.body.querySelector('.practice-container .box')
@@ -6,6 +50,7 @@ container.addEventListener('click',function(e){
         choose = e.target.textContent.toLowerCase()
         container.classList.add('display')
         practiceBox.classList.remove('display')
+        timerElement.classList.remove('display')
         getKana(choose)
     }
 })
@@ -34,8 +79,9 @@ const input = document.body.querySelector('.box .bottom input')
 function getRandomKeyAndDelete(kana,choose) {
     const keys = Object.keys(kana);
     if (keys.length === 0) {
+        stopTimer()
         box_kana.innerHTML = "Selesai"
-        input.value = correctAnswer + "/" + totalQuestion
+        input.value = timerElement.textContent
         input.readOnly = true
         return;
     }
@@ -62,10 +108,12 @@ async function getKana(choose) {
 
 }
 
-
-
 input.addEventListener('keyup',function(){
-    if(input.value === tempKey){
+    // Start a timer that repeats a function at a specified interval
+    if(!start){
+        startTimer()
+    }
+    if(input.value.toLowerCase() === tempKey){
         input.value =''
         correctAnswer++
         getRandomKeyAndDelete(kana,choose)
