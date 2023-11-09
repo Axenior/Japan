640775f786300daefae6e49693173db161e6c35a
MatiusStephen <matiusstephen813@gmail.com>
2023-11-09 20:09:27 +0700

    Menambah fitur latihan kosakata

--- latihan/latihan.js
+++ latihan/latihan.js
@@ -4,6 +4,15 @@ let minutes = 0;
 let hours = 0;
 let intervalId;
 
+let kana;
+let kosakata;
+let tempKosakata;
+let tempKey;
+let noQuestion = 0;
+let correctAnswer = 0
+let totalQuestion;
+let totalSelectQuestion;
+
 function startTimer(){
     intervalId = setInterval(function() {
         // seconds++
@@ -20,7 +29,6 @@ function stopTimer(){
 
 const timerElement = document.body.querySelector('.timer')
 function updateTimer() {
-    // Increment the seconds
     seconds++;
 
     // Update minutes and reset seconds if necessary
@@ -44,7 +52,11 @@ function updateTimer() {
 
 let choose;
 const container = document.body.querySelector('.container')
+const totalQuestionElement = document.body.querySelector('.total-question')
+const rangeQuestion = document.body.querySelector('#range-question')
+
 const practiceBox = document.body.querySelector('.practice-container .box')
+const practiceKosakata = document.body.querySelector('.practice-container .kosakata')
 container.addEventListener('click',function(e){
     if(e.target.parentNode.classList.contains('content1')){
         choose = e.target.textContent.toLowerCase()
@@ -53,7 +65,40 @@ container.addEventListener('click',function(e){
         timerElement.classList.remove('display')
         getKana(choose)
     }
+    else if(e.target.parentNode.classList.contains('content2')){
+        choose = e.target.textContent.toLowerCase()
+
+        const handleAsyncOperation = async () => {
+            await getTotalQuestion(choose);
+            
+            // Continue with the rest of your code here
+            container.classList.add('display');
+
+            rangeQuestion.innerHTML = `1 - ${totalQuestion}`
+            totalQuestionElement.classList.remove('display');
+            // practiceKosakata.classList.remove('display');
+            // getKosakata(choose);
+        };
+        
+        handleAsyncOperation();
+    }
 })
+
+const totalInput = document.body.querySelector('#total-input')
+const submitButton = document.body.querySelector('#submit')
+
+submitButton.addEventListener('click',function(){
+    if(totalInput.value > 0 && totalInput.value <= totalQuestion){
+        totalSelectQuestion = totalInput.value
+        totalQuestionElement.classList.add('display')
+        practiceKosakata.classList.remove('display');
+        getKosakata(choose);
+    }else{
+        const alert = document.body.querySelector('.alert')
+        alert.classList.remove('display')
+    }
+})
+
 async function fetchKana(){
     try {
         const response = await fetch("/kana/kana.json");
@@ -68,15 +113,25 @@ async function fetchKana(){
     }
 }
 
-let kana;
-let tempKey;
-let correctAnswer = 0
-let totalQuestion;
+async function fetchKosakata(x){
+    try{
+        const response = await fetch("/kosakata/"+x+".json");
+        if(!response.ok){
+            throw new Error("Network response was not ok")
+        }
+        const data = await response.json();
+        return data;
+    }catch (error){
+        console.log("Error Loading JSON data : ",error)
+        return null;
+    }
+}
+
 
 const box_kana = document.querySelector('.box .top h1')
 const input = document.body.querySelector('.box .bottom input')
 
-function getRandomKeyAndDelete(kana,choose) {
+function getRandomKeyAndDeleteKana(kana,choose) {
     const keys = Object.keys(kana);
     if (keys.length === 0) {
         stopTimer()
@@ -92,10 +147,10 @@ function getRandomKeyAndDelete(kana,choose) {
     // Get the corresponding value for the random key
     const randomValue = kana[randomKey];
     
-    // Delete the key-value pair from the object
-    
     tempKey = randomKey
     box_kana.innerHTML = randomValue[choose]
+
+    // Delete the key-value pair from the object
     delete kana[randomKey];
 }
 
@@ -103,9 +158,8 @@ async function getKana(choose) {
     kana = await fetchKana();
     totalQuestion = Object.keys(kana).length
     if(kana){
-        getRandomKeyAndDelete(kana,choose);
+        getRandomKeyAndDeleteKana(kana,choose);
     }
-
 }
 
 input.addEventListener('keyup',function(){
@@ -116,6 +170,124 @@ input.addEventListener('keyup',function(){
     if(input.value.toLowerCase() === tempKey){
         input.value =''
         correctAnswer++
-        getRandomKeyAndDelete(kana,choose)
+        getRandomKeyAndDeleteKana(kana,choose)
     }
 })
+
+const numberElement = document.querySelector('.number-kanji')
+const kanji = document.body.querySelector('.kosakata .kanji')
+// console.log(kanji)
+function getRandomKeyAndDeleteKosakata(kosakata) {
+    const keys = Object.keys(kosakata);
+    if (noQuestion == totalSelectQuestion) {
+        const scoreElement = document.body.querySelector('.score')
+        const numberScore = document.body.querySelector('.number-score p')
+        numberScore.innerHTML = `${correctAnswer}/${numberElement.textContent}`
+        kanji.innerHTML = "Selesai"
+        kanji.classList.add('finish')
+        scoreElement.classList.remove('display')
+        optionContainer.classList.add('display')
+        numberElement.classList.add('display')
+        return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    const randomKey = keys[randomIndex];
+    
+    // Get the corresponding value for the random key
+    const randomValue = kosakata[randomKey];
+
+    tempKey = randomKey
+    noQuestion++
+    numberElement.innerHTML = noQuestion
+    kanji.innerHTML = randomValue['kanji']
+    getOption(tempKey)
+    // Delete the key-value pair from the object
+    delete kosakata[randomKey];
+}
+
+async function getKosakata(x){
+    kosakata = await fetchKosakata(x);
+    tempKosakata = await fetchKosakata(x);
+    totalQuestion = Object.keys(kosakata).length
+    if(kosakata){
+        getRandomKeyAndDeleteKosakata(kosakata);
+        // return kosakata;
+    }
+}
+
+async function getTotalQuestion(x){
+    kosakata = await fetchKosakata(x);
+    totalQuestion = Object.keys(kosakata).length
+}
+
+
+const optionElements = document.body.querySelectorAll('.option')
+function getOption(answerKey){
+    const answer = answerKey
+    // const tempOption = tempKosakata
+    const tempOption = Object.assign({}, tempKosakata)
+
+    delete tempOption[answer]
+
+    let keys = Object.keys(tempOption);
+    let randomIndex = Math.floor(Math.random() * keys.length);
+    let randomKey = keys[randomIndex];
+    const option1 = randomKey
+    delete tempOption[randomKey]
+
+    keys = Object.keys(tempOption);
+    randomIndex = Math.floor(Math.random() * keys.length);
+    randomKey = keys[randomIndex];
+    const option2 = randomKey
+
+    let options = [answer,option1,option2]
+    optionElements.forEach(function(element){
+        const randomIndex = Math.floor(Math.random() * options.length)
+        element.children[0].innerHTML = options[randomIndex]
+        element.children[1].innerHTML = tempKosakata[options[randomIndex]]['arti']
+        // console.log(tempKosakata[options[randomIndex]]['arti'])
+
+        options = options.slice(0, randomIndex).concat(options.slice(randomIndex + 1))
+        // console.log(options)
+    })
+}
+
+const optionContainer = document.body.querySelector('.option-container')
+
+optionContainer.addEventListener('click', handleClick);
+
+function handleClick(e) {
+  let target = e.target;
+  
+  while (target && !target.classList.contains('option')) {
+    target = target.parentElement;
+  }
+  if(target && target.classList.contains('option')){
+      // Menonaktifkan event listener saat sedang berjalan
+      optionContainer.removeEventListener('click', handleClick);
+      
+      if (target.firstElementChild.textContent == tempKey) {
+          correctAnswer++;
+        }
+        optionElements.forEach(function(element){
+            if(element.firstElementChild.textContent == tempKey){
+                element.classList.add('correct')
+            }else{
+                element.classList.add('wrong')
+            }
+        })
+        setTimeout(function () {
+            optionElements.forEach(function(element){
+                element.classList.remove('correct')
+                element.classList.remove('wrong')
+            })
+            getRandomKeyAndDeleteKosakata(kosakata);
+            
+            optionContainer.addEventListener('click', handleClick);
+        }, 1000);
+    }
+
+//   console.log(target);
+}
+
